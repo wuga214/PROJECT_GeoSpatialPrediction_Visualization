40fc66076e18af8f6717c10017b7d94c75b23355
wuga214 <wuga214@gmail.com>
2015-12-05 19:49:13 -0800

    Code Modification
    Cleaned up useless part of this code and only left visualization part

--- SFHousePrice/src/BMAUtil/__init__.py
+++
@@ -1,3 +0,0 @@
-from merge import merge
-from vorpartition import partition
-from pointpolygon import point_in_poly
--- SFHousePrice/src/Classifiers/__init__.py
+++
@@ -1,4 +0,0 @@
-from knn import knnregression
-from gaussianprocess import gpregression
-from evaluator import cut_loca_price
-from evaluator import mse_eval
